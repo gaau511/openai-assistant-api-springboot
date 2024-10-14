4d22a89f5fd848ada1d3208c7f9d590d1f28aaa3
gaau511 <gaau511@gmail.com>
2024-10-14 18:16:23 +0900

    create assistant dto & test

---
+++ src/main/java/com/spring/openaiassistant/dto/AssistantDto.java
@@ -0,0 +1,166 @@
+package com.spring.openaiassistant.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AssistantDto {
+
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("object")
+    private String object;
+
+    @JsonProperty("created_at")
+    private Integer createdAt;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("description")
+    private String description;
+
+    @JsonProperty("model")
+    private String model;
+
+    @JsonProperty("instructions")
+    private String instructions;
+
+    @JsonProperty("tools")
+    private List<Tool> tools;
+
+    @JsonProperty("tool_resources")
+    private ToolResources toolResources;
+
+    @JsonProperty("metadata")
+    private Map<String, Object> metadata;
+
+    @JsonProperty("temperature")
+    private Double temperature;
+
+    @JsonProperty("top_p")
+    private Double topP;
+
+    // string : "auto" or ResponseFormat
+    @JsonProperty("response_format")
+    private Object responseFormat;
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Tool {
+        @JsonProperty("type")
+        private String type;
+
+        @JsonProperty("file_search")
+        private FileSearch fileSearch;
+
+        @JsonProperty("function")
+        private Function function;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class FileSearch {
+        @JsonProperty("max_num_results")
+        private Integer maxNumResults;
+
+        @JsonProperty("ranking_options")
+        private RankingOptions rankingOptions;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class RankingOptions {
+        @JsonProperty("ranker")
+        private String ranker;
+
+        @JsonProperty("score_threshold")
+        private Double scoreThreshold;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Function {
+        @JsonProperty("description")
+        private String description;
+
+        @JsonProperty("name")
+        private String name;
+
+        @JsonProperty("parameters")
+        private Map<String, Object> parameters;
+
+        @JsonProperty("strict")
+        private Boolean strict;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ToolResources {
+        @JsonProperty("code_interpreter")
+        private CodeInterpreter codeInterpreter;
+
+        @JsonProperty("file_search")
+        private FileSearch fileSearch;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CodeInterpreter {
+        @JsonProperty("file_ids")
+        private List<String> fileIds;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    // use via casting
+    public static class ResponseFormat {
+        @JsonProperty("type")
+        private String type;
+
+        @JsonProperty("json_schema")
+        private JsonSchema jsonSchema;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class JsonSchema {
+        @JsonProperty("description")
+        private String description;
+
+        @JsonProperty("name")
+        private String name;
+
+        @JsonProperty("schema")
+        private Map<String, Object> schema;
+
+        @JsonProperty("strict")
+        private Boolean strict;
+    }
+}
---
+++ src/test/java/com/spring/openaiassistant/dto/AssistantDtoTest.java
@@ -0,0 +1,38 @@
+package com.spring.openaiassistant.dto;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AssistantDtoTest {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void test() {
+        String exampleAssistantObject = "{\n" +
+                "  \"id\": \"asst_abc123\",\n" +
+                "  \"object\": \"assistant\",\n" +
+                "  \"created_at\": 1698984975,\n" +
+                "  \"name\": \"Math Tutor\",\n" +
+                "  \"description\": null,\n" +
+                "  \"model\": \"gpt-4o\",\n" +
+                "  \"instructions\": \"You are a personal math tutor. When asked a question, write and run Python code to answer the question.\",\n" +
+                "  \"tools\": [\n" +
+                "    {\n" +
+                "      \"type\": \"code_interpreter\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"metadata\": {},\n" +
+                "  \"top_p\": 1.0,\n" +
+                "  \"temperature\": 1.0,\n" +
+                "  \"response_format\": \"auto\"\n" +
+                "}\n";
+
+        assertDoesNotThrow(() -> {AssistantDto assistant = objectMapper.readValue(exampleAssistantObject, AssistantDto.class);});
+    }
+
+}
